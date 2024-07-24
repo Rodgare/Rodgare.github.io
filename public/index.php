7ffde02ee110bf12ea2e7878fc2fb7371810e128
Axel <axel19911@gmail.com>
2024-07-24 18:47:54 +0300

    3

--- public/index.php
+++ public/index.php
@@ -5,12 +5,21 @@ namespace App\Index;
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
-$app->addErrorMiddleware(true, true, true);
-
-$app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+
+$app->get('/users', function ($request, $response, $args) use ($users) {
+    $term = $request->getQueryParam('term');
+    $filteredUsers = array_filter($users, fn ($user) => str_contains($user, $term));
+    $params = ['users' => $filteredUsers, 'term' => $term];
+    return $this->get('renderer')->render($response, '../templates/users/index.phtml', $params);
+});
+
 $app->run();
